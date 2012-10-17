cdef6ad41deeedd5e477ae719e7a609a2213e8d6
Kyle <go4ble23@gmail.com>
2012-10-16 20:46:20 -0700

    works locally

--- bulletin_project/bulletin_project/settings.py
+++ bulletin_project/bulletin_project/settings.py
@@ -12,8 +12,8 @@ MANAGERS = ADMINS
 # dj_database_url overrite at bottom
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': './bull.db',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -152,5 +152,5 @@ LOGGING = {
     }
 }
 
-import dj_database_url
-DATABASES['default'] = dj_database_url.config()
+#import dj_database_url
+#DATABASES['default'] = dj_database_url.config()
