cae9b587638f23fa3bedd9e2602df6748b234321
Kyle <go4ble23@gmail.com>
2012-10-17 16:47:27 -0700

    trying to figure out this static dir stuff

--- bulletin_project/settings.py
+++ bulletin_project/settings.py
@@ -56,7 +56,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = 'static_root/'
+STATIC_ROOT = 'static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -67,7 +67,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    'static/',
+    'templates/static',
 )
 
 # List of finder classes that know how to find static files in
