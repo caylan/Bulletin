933e703c6f93a4d4ae1f8b9eaacccb6b777b9f05
Kyle <go4ble23@gmail.com>
2012-10-18 22:06:55 -0700

    added ability to post

--- bulletin_project/settings.py
+++ bulletin_project/settings.py
@@ -91,7 +91,8 @@ TEMPLATE_LOADERS = (
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # Probably should uncomment this in the future
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     # Uncomment the next line for simple clickjacking protection:
