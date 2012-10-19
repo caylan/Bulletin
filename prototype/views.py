a94d82ac62a2265d017ad058a855cde104573dc8
Kyle <go4ble23@gmail.com>
2012-10-18 21:13:54 -0700

    git er done

--- prototype/views.py
+++ prototype/views.py
@@ -2,13 +2,6 @@ from django.shortcuts import render_to_response, get_list_or_404
 from prototype.models import Post, Comment
 
 def index(request):
-    """
-    post_list = Post.objects.all()
-    discussion = []
-    for post in post_list:
-        discussion.append((post, post.comment_set.all()))
-    return render_to_response('prototype/index.html', {'discussion': discussion})
-    """
     return render_to_response('prototype/index.html')
 
 def group(request, grpid):
