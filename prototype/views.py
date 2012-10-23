e04270c7c30d4550140b6dc88e514a32f837161c
Kyle <go4ble23@gmail.com>
2012-10-23 14:59:35 -0700

    whole post is now shown

--- prototype/views.py
+++ prototype/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render_to_response, get_list_or_404, redirect
 from prototype.models import Post, Comment, PostForm
+import md5
 
 def index(request):
     return render_to_response('prototype/index.html')
@@ -19,4 +20,6 @@ def group(request, grpid):
     
     # post_list = get_list_or_404(Post, group=grpid)
     post_list = list(Post.objects.filter(group=grpid))
-    return render_to_response('prototype/group.html', {'post_list': post_list, 'grpid': grpid, 'form': form})
+    return render_to_response('prototype/group.html', {'post_list': post_list,
+                                                       'grpid': grpid,
+                                                       'form': form})
