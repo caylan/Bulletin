d8c9f0280f9a00a50c8ce7e6b49b45a9ba69e33d
Andrew Davies <andavies@cs.washington.edu>
2012-10-30 13:37:11 -0700

    Fixed naming problems.

---
+++ bulletin_project/urls.py
@@ -0,0 +1,21 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'bulletin_project.views.home', name='home'),
+    # url(r'^bulletin_project/', include('bulletin_project.foo.urls')),
+    url(r'^prototype/$', 'groups.views.index'),
+    url(r'^prototype/group/(?P<grpid>\d+)/$', 'groups.views.group'),
+    url(r'^prototype/login/$', 'session.views.login'),
+    url(r'^prototype/register/$', 'registration.views.register'),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+)
---
+++ posts/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+import models
+
+# Posts
+class CommentInline(admin.TabularInline):
+    model = models.Comment
+
+class PostAdmin(admin.ModelAdmin):
+    fields = ('author', 'date_posted', 'message', 'group')
+    readonly_fields = ('date_posted',)
+    inlines = (CommentInline,)
+
+admin.site.register(models.Post, PostAdmin)
+
+###
