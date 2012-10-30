d8c47359dd7070d6d066b37c0a08ef60cb86eeb3
Andrew Davies <andavies@cs.washington.edu>
2012-10-30 00:04:57 -0700

    Fixed spelling error.

--- registration/views.py
+++ registration/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render_to_response, get_list_or_404, redirect
 from forms import RegistrationForm
 
 def register(request):
-    form = RegisterForm()
+    form = RegistrationForm()
     return render_to_response('prototype/register.html', {'form': form,})
