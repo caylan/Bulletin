29c73f717e2dacf64307a9d34ccd2eff253b830a
Kyle Boone <boonekr@cs.washington.edu>
2012-10-29 17:06:37 -0700

    started admin site

---
+++ bulletin_project/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from bulletin import models
+
+# Posts
+class CommentInline(admin.TabularInline):
+    model = models.Comment
+
+class PostAdmin(admin.ModelAdmin):
+    fields = ('author', 'date_posted', 'message',)
+    readonly_fields = ('date_posted',)
+    inlines = (CommentInline,)
+
+admin.site.register(Post, PostAdmin)
+
+###
