3b7f84e854f9ee08ccdcb8534472c6142faab3d2
Andrew Davies <andavies@cs.washington.edu>
2012-10-29 20:56:09 -0700

    Added some comments on the (not completed) user creation form.

--- bulletin/forms.py
+++ bulletin/forms.py
@@ -1,6 +1,13 @@
 from django.contrib.auth.forms import UserCreationForm
 
 class BulletinUserCreationForm(UserCreationForm):
+    '''
+    This will create a user once all of the criteria
+    has been met.
+
+    For now, the only forced criteria are that the email is required,
+    but it should also require the first and last name.
+    '''
     
     def __init__(self, *args, **kwargs):
         super(BulletinUserCreationForm , self).__init__(*args, **kwargs)
