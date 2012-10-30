90cb56e3890bcbec45b993bda7ee920f58732a92
Andrew Davies <andavies@cs.washington.edu>
2012-10-30 15:18:26 -0700

    Added crude login.

--- session/forms.py
+++ session/forms.py
@@ -3,21 +3,5 @@ from django.contrib.auth.models import User
 from django.utils.translation import ugettext, ugettext_lazy as _
 
 class LoginForm(forms.Form):
-    error_messages = {
-        'invalid' : _("Invalid user name or password"),
-    }
-    email = forms.CharField(label='Email')
-    password = forms.CharField(widget=forms.PasswordInput, label='Password')
-
-    class Meta:
-        fields = ('email', 'password')
-
-    def clean_user(self):
-        email = self.cleaned_data['email']
-        try:
-            user = User.objects.get(email=email)
-            if user.check_password(self.cleaned_data['password']):
-                return user
-        except User.DoesNotExist:
-            pass
-        raise forms.ValidationError(self.error_messages['invalid'])
+    email = forms.EmailField(label='Email', max_length=40, required=True)
+    password = forms.CharField(widget=forms.PasswordInput, label='Password', required=True)
--- session/views.py
+++ session/views.py
@@ -1,13 +1,37 @@
 from django.shortcuts import render_to_response, get_list_or_404, redirect
+from django.http import HttpResponse
+from django.contrib.auth.models import User
 from forms import LoginForm
 
+__VALID__   = "You've logged in successfully!"
+
 def login(request):
+    '''
+    This is a bit of a hack, but essentially we attempt to log in the
+    user, and if they have an invalid password or email, then the
+    form is labeled as invalid for the associated template.
+
+    If the template is valid, then (for the moment) we create a user
+    session and redirect them to a useless web page that doesn't do
+    anything.
+    '''
+    valid = True
     if request.method == 'POST':
         form = LoginForm(request.POST)
         if form.is_valid():
-            '''
-            If we have a valid form, then we create a user session.
-            '''
-            request.session['user'] = form.clean_user()
-    form = LoginForm()
-    return render_to_response('login.html', {'form' : form,})
+            try:
+                user = User.objects.get(email=request.POST['email'])
+                if user.check_password(request.POST['password']):
+                    request.session["user_id"] = user.id
+                    ''' This needs to send the user to the main page! '''
+                    return HttpResponse(__VALID__)
+                else:
+                    valid = False
+            except User.DoesNotExist:
+                valid = False
+                pass
+        else:
+            valid = False
+    else:
+        form = LoginForm()
+    return render_to_response('login.html', {'form' : form, 'valid' : valid})
