fd205bc08f6ac8e8622df50569135ceb0a21c3b3
Andrew Davies <andavies@cs.washington.edu>
2012-10-30 00:35:04 -0700

    Changed registration form.

--- registration/forms.py
+++ registration/forms.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User
+from django import forms
 from django.contrib.auth.forms import UserCreationForm
 
 class RegistrationForm(UserCreationForm):
@@ -6,9 +7,11 @@ class RegistrationForm(UserCreationForm):
     A form that creates a user, using all of the required
     fields.
     '''
+    email = forms.EmailField(label='Email', max_length=40)
+
     class Meta:
         model = User
-        fields = ('first_name', 'last_name')
+        fields = ('email', 'first_name', 'last_name')
 
     def clean_email(self):
         ''' Checks to see if a user with the following email exists '''
@@ -24,7 +27,7 @@ class RegistrationForm(UserCreationForm):
         user = super(UserCreationForm, self).save(commit=False)
         user.set_password(self.cleaned_data["password"])
         user.email = self.cleaned_data["email"]
-        user.is_active = False # false until email is activated!
+        user.is_active = False # false until email activation!
         if commit:
             user.save()
         return user
