a05190070957d2771117433cbcd35db92f6b64d8
Andrew Davies <andavies@cs.washington.edu>
2012-10-30 22:51:48 -0700

    Added email and invite confirmation (incomplete).

--- registration/views.py
+++ registration/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render_to_response, get_list_or_404, redirect
+from models import (
+    EmailConfirmationManager,
+    EmailInvite,
+    EmailConfirmation,
+    AbstractEmailConfirmation
+)
 from forms import RegistrationForm
 
 def register(request):
@@ -15,5 +21,13 @@ def register(request):
         form = RegistrationForm()
     return render_to_response('register.html', {'form': form,})
 
-def confirm_email(request):
-    pass
+def confirm_email(request, key):
+    '''
+    This confirms an email that has been sent by the website.
+    This is strictly for email confirmations, not for invites from
+    other users (those will be added later).
+    '''
+    confirmation_key = confirmation_key.lower()
+    email = AbstractEmailConfirmation.objects.confirm_email(key)
+    return render_to_response('confirm_email/(
+
