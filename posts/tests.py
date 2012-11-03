3ea21f06841fd9d8b02812d580ea3839b693a75d
Brian-Oliphant <oliphb@uw.edu>
2012-11-03 01:47:52 -0700

    Unit testing for posts

--- posts/tests.py
+++ posts/tests.py
@@ -1,16 +1,71 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
+from django.utils import timezone
+from posts.models import Post
+from posts.models import Comment
+from groups.models import Group
+from django.contrib.auth.models import User
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class GroupModelTest(TestCase):
+    def test_post(self):
+        post = Post()
+        
+        user = User.objects.create_user('Testy McTest', 'test@test.com', 'testpassword')
+        user.save()
+        
+        group = Group()
+        group.name = "Test Group"
+        group.save()
+        
+        post.author = User.objects.get(id = user.id)
+        post.message = "Testing321"
+        post.group = Group.objects.get(id = group.id)
+
+        post.save()
+    
+        test_post = Post.objects.get(id = post.id)
+        
+        self.assertEquals(test_post, post)
+        self.assertEquals(test_post.author, User.objects.get(id = user.id))
+        self.assertEquals(test_post.message, "Testing321")
+        self.assertEquals(test_post.group, Group.objects.get(id = group.id))
+   
+        post.delete()
+        group.delete()
+        user.delete()
+        
+        
+    def test_comment(self):
+        comment = Comment()
+        post = Post()
+        
+        user = User.objects.create_user('Testy McTest', 'test@test.com', 'testpassword')
+        user.save()
+        
+        group = Group()
+        group.name = "Test Group"
+        group.save()
+        
+        post.author = User.objects.get(id = user.id)
+        post.message = "Testing 321"
+        post.group = Group.objects.get(id = group.id)
+        post.save()
+        
+        comment.author = User.objects.get(id = user.id)
+        comment.message = " 123Testing"
+        comment.group = Group.objects.get(id = group.id)
+        comment.post = Post.objects.get(id = post.id)
+
+        comment.save()
+    
+        test_comment = Comment.objects.get(id = post.id)
+        
+        self.assertEquals(test_comment, comment)
+        self.assertEquals(test_comment.author, User.objects.get(id = user.id))
+        self.assertEquals(test_comment.message, "Testing321")
+        self.assertEquals(test_comment.group,  Group.objects.get(id = group.id))
+   
+        comment.delete()
+        post.delete()
+        group.delete()
+        user.delete()
