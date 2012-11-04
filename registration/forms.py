f3a2d33ade2653e39e89b21eb44638d5f82a39b2
Andrew Davies <andavies@cs.washington.edu>
2012-11-03 17:27:39 -0700

    A bug fix for sending multiple email confirmations.

--- registration/forms.py
+++ registration/forms.py
@@ -43,10 +43,6 @@ class RegistrationForm(forms.ModelForm):
         if user.is_active:
             raise forms.ValidationError(self.error_messages['duplicate_email'])
         else:
-            # User is deleted as the email is the same.  A new user is constructed
-            # when calling self.save()
-            # TODO: Is this really a good idea?
-            user.delete()
             return email
 
     def clean_password2(self):
@@ -63,9 +59,16 @@ class RegistrationForm(forms.ModelForm):
         is indeed a little bit of a hack, so maybe this can be changed
         later by making a custom User class (like what was planned
         in the first place).
+
+        If the user still exists but is not active, then we simply
+        return the user again, since we're going to send another
+        validation in the view.py method (at least, that's what we're supporting).
         '''
-        user = super(RegistrationForm, self).save(commit=False)
-        user.username = self.cleaned_data['email']
+        try:
+            user = User.objects.get(email=self.cleaned_data['email'])
+        except User.DoesNotExist:
+            user = super(RegistrationForm, self).save(commit=False)
+            user.username = self.cleaned_data['email']
 
         ''' Don't activate a user until the email is sent! '''
         user.is_active = False 
