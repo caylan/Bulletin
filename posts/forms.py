21cf02d0f8caf28c59b6f515c216d556d6c38faf
Andrew Davies <andavies@cs.washington.edu>
2012-11-05 22:36:09 -0800

    Disallowed whitespace in message posting.

--- posts/forms.py
+++ posts/forms.py
@@ -1,10 +1,12 @@
-from django.forms import ModelForm
+from django import forms
 from models import Post
 
-class PostForm(ModelForm):
+class PostForm(forms.ModelForm):
     """
     Form for the post model
     """
+    message = forms.RegexField(regex=r'[^\s]',)
+
     class Meta:
         model = Post
         # the following should be set by the view depending on context
