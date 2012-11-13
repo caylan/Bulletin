460efdb7c59e8b9d1a6736b102057487de648bb3
Brian-Oliphant <oliphb@uw.edu>
2012-11-13 01:41:56 -0800

    Selenium tests! Doesn't work yet because of fixture

--- session/tests.py
+++ session/tests.py
@@ -1,16 +1,34 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
-from django.test import TestCase
+from django.test import LiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class SessionsTest(LiveServerTestCase):
+    fixtures = ['admin_user.json']
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_log_in_and_out(self):
+        self.browser.get(self.live_server_url)
+        
+        # admin email
+        email_field = self.browser.find_element_by_name('email')
+        email_field.send_keys('a@b.com')
+
+        #admin password
+        password_field = self.browser.find_element_by_name('password')
+        password_field.send_keys('admin')
+        password_field.send_keys(Keys.RETURN)
+
+        header = self.browser.find_element_by_tag_name('h1')
+        self.assertIn('inbox', body.text)
+
+        self.browser.find_elements_by_link_text('Logout').click()
+
+        header = self.browser.find_element_by_tag_name('h1')
+        self.assertIn('Bulletin', body.text)
