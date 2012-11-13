fde26e57b95fd2b1f64cb4fec4f97d57bd238a00
Kyle Boone <boonekr@cs.washington.edu>
2012-11-12 19:33:20 -0800

    ajax comments now work :)

--- posts/urls.py
+++ posts/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns("posts.views",
     url(r'^post/(?P<postid>\d+)/comment/$', 'comment'),
+    url(r'^delete_me/$', 'delete_me'),
 )
--- posts/views.py
+++ posts/views.py
@@ -2,8 +2,10 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseBadRequest
 from django.core import serializers
+from django.shortcuts import render
 from forms import CommentForm
 from models import Comment, Post
+from groups.models import Group
 
 @login_required
 def comment(request, postid):
@@ -16,10 +18,15 @@ def comment(request, postid):
             comment = form.save(commit=False)
             # author = from the current user's set of memberships, the one that
             #          has a group with matching group id (pk)
-            comment.author = request.user.membership_set.get(group__pk=grpid)
+            comment.author = request.user.membership_set.get(group__membership__post__pk=postid)
             # TODO try catch statement
             comment.post = Post.objects.get(pk=postid)
             comment.save()
-            return HttpResponse(serializers.serialize('xml', comment), mimetype='text/xml')
+            return HttpResponse(serializers.serialize('xml', [comment,]), mimetype='text/xml')
     else:
         return HttpResponseBadRequest()
+
+@login_required
+def delete_me(request):
+    post_list = Post.objects.all()
+    return render(request, 'delete_me.html', {'post_list': post_list,})
