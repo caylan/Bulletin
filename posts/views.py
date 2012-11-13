35de40b9e6c873f200c0adc7228638e2fe99ed00
Kyle <go4ble23@gmail.com>
2012-11-13 14:51:40 -0800

    ajax post

--- posts/views.py
+++ posts/views.py
@@ -2,13 +2,12 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseBadRequest
 from django.shortcuts import render
-from forms import CommentForm
+from forms import CommentForm, PostForm
 from models import Comment, Post
 from groups.models import Group
 
 @login_required
 def comment(request, postid):
-    post = Post.objects.get(pk=postid)
     # TODO check authorization
 
     if request.method == 'POST':
@@ -21,8 +20,20 @@ def comment(request, postid):
             comment.post = Post.objects.get(pk=postid)
             comment.save()
             return HttpResponse(comment.json(), mimetype='application/json')
-    else:
-        return HttpResponseBadRequest()
+    return HttpResponseBadRequest()
+
+@login_required
+def post(request, grpid):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            # author = from the current user's set of memberships, the one that
+            #          has a group with matching group id (pk)
+            post.author = request.user.membership_set.get(group__pk=grpid)
+            post.save()
+            return HttpResponse(post.json(), mimetype='application/json')
+    return HttpResponseBadRequest()
 
 @login_required
 def delete_me(request):
