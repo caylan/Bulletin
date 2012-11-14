1a123ecce01509e25066bf151872ca91a5027bf1
Ivan Darmansyah <blinkerz@gmail.com>
2012-11-13 21:20:54 -0800

    Added loading spinner when the user ajax-registers

--- static_resources/js/register.js
+++ static_resources/js/register.js
@@ -1,33 +1,19 @@
 function register() {
 	var containerInPage = $('#content-container');
-	var csrfMiddlewareToken = $('input[type=hidden]').val();
-	var firstName = $('#id_first_name').val();
-	var lastName = $('#id_last_name').val();
-	var email = $('#id_email').val();
-	var password1 = $('#id_password1').val();
-	var password2 = $('#id_password2').val();
-	
-	$.ajax({
-		type: "POST",
-		url: ".",
-		data: {
-			csrfmiddlewaretoken: csrfMiddlewareToken,
-			first_name: firstName,
-			last_name: lastName,
-			email: email,
-			password1: password1,
-			password2: password2
-		}
-	}).done(
-			function(output) {
-				var nextPage = $($("#content-container", output)[0]);
-				containerInPage.fadeOut(function() {
-					containerInPage.html(nextPage.html());
-					containerInPage.find('ul.errorlist').addClass("alert alert-error")
-					ajaxRegister();
-				});
-				containerInPage.fadeIn();
+	$('.loading-spinner').show();
+	$.post(
+		".",
+		$('.form-signin').serialize(),
+		function(output) {
+			var nextPage = $("#content-container", output);
+			containerInPage.fadeOut(function() {
+				containerInPage.html(nextPage.html());
+				containerInPage.find('ul.errorlist').addClass("alert alert-error")
+				ajaxRegister();
 			});
+			containerInPage.fadeIn();
+		}
+	);
 }
 
 function ajaxRegister() {
