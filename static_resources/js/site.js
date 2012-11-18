2a5103a0ff7138cb736ebb62b7162ec18970ab37
Ivan Darmansyah <blinkerz@gmail.com>
2012-11-17 19:04:26 -0800

    Hotfix IE bug

--- static_resources/js/site.js
+++ static_resources/js/site.js
@@ -1,28 +1,31 @@
 $(document).ready(function() {
-    $('.tabs-left').children().each(function() {
-        $(this).css("height", $(this).parent().css("height"));
-    });
     // Find ALL <form> tags
     $('form').submit(function(){
         // On submit disable its submit button
-        $('.submit-btn', this).attr('disabled', 'disabled');
+        $('.submit-btn', this).attr("disabled", "disabled");
     });
 });
 
+// Google Analytics
 var _gaq = _gaq || [];
 _gaq.push(['_setAccount', 'UA-36110349-1']);
 _gaq.push(['_setDomainName', 'bulletinapp.net']);
 _gaq.push(['_trackPageview']);
 
 (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	var ga = document.createElement('script');
+	ga.type = 'text/javascript';
+	ga.async = true;
+	ga.src = ('https:' == document.location.protocol ? 'https://ssl'
+			: 'http://www')
+			+ '.google-analytics.com/ga.js';
+	var s = document.getElementsByTagName('script')[0];
+	s.parentNode.insertBefore(ga, s);
 })();
 
 $(window).scroll(function(){
     if ($(window).scrollTop() != 0){
-        $("#jump-top").show(500);
+        $("#jump-top").css("display", "inline-block");
     } else {
         $("#jump-top").hide();
     }
