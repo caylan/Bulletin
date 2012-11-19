349a787420efe0e03e35a0093d6790c87a06dfa3
Ivan Darmansyah <blinkerz@gmail.com>
2012-11-18 20:07:17 -0800

    Inviting people directly from create_group_modal

--- static_resources/js/create-group.js
+++ static_resources/js/create-group.js
@@ -1,5 +1,36 @@
+var count = 0;
+
+function isEmail(email) {
+	var regex = /^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+	return regex.test(email);
+}
+
 function createGroup() {
 	var $modal = $('#create-group');
+	var emailArray = [];
+	
+	if (count > 0) {
+		var $input;
+		var email;
+		
+		// TODO: make the array
+		for (var i = 0; i < count; i++) {
+			$input = $('input[name="email' + i + '"]');
+			email = $.trim($input.val());
+			
+			if (email == "" || !isEmail(email)) {
+				continue;
+			}
+			
+			emailArray.push(email);
+		}
+		console.log(emailArray);
+		// clean up so nothing gets submitted by the form
+		count = 0;
+		$('.people-list-header').hide();
+		$('.add-invitee-btn').siblings('.people-list').html("");
+	}
+	
 	$.post(
 		"/create/",
 		$('#create-group-form').serialize(),
@@ -8,11 +39,12 @@ function createGroup() {
 				window.location.replace(output.location);
 			} else {
 				$modal.fadeOut(function() {
+					count = 0;
 					$modal.html($(output).html());
 					$modal.find('ul.errorlist').addClass("alert alert-error")
 					ajaxCreateGroup();
 					if($.browser.msie && parseInt($.browser.version, 10) < 10) {
-						$('input, textarea').placeholder();
+						$modal.find('input, textarea').placeholder();
 					}
 				});
 				$modal.fadeIn();
@@ -21,16 +53,42 @@ function createGroup() {
 	);
 }
 
+function addNewPersonField() {
+	// id=\"email" + count + "\"
+	var fieldHtml = "<input type=\"text\" name=\"email" + count + "\" placeholder=\"Email Address " + count + "\" style=\"display: none;\" />";
+	var $input = $(fieldHtml);
+	var $peopleList = $('.add-invitee-btn').siblings('.people-list');
+	$peopleList.append($input);
+	$input.show("blind", function() {
+		$peopleList.append($("<br />"))
+	});
+	count++;
+}
+
 function ajaxCreateGroup() {
 	$('#create-group-form').submit(function(event) {
 		event.preventDefault();
 		createGroup();
 	});
 	
-	$('#create-group-form a.btn, #create-group-form .close').click(function() {
+	$('#create-group-form a.btn.dismiss, #create-group-form .close').click(function() {
 		$('#create-group-form input[type="text"]').val("");
 		$('#create-group-form .errorlist').remove();
+		count = 0;
+		$('.people-list-header').hide();
+		$('.add-invitee-btn').siblings('.people-list').html("");
 	});
+	
+	$('.add-invitee-btn').click(function() {
+        if (count == 0) {
+            $('.people-list-header').show("blind", function () {
+            	addNewPersonField();
+            });
+        } else {
+        	addNewPersonField();
+        }
+        
+    });
 }
 
 $(document).ready(function() {
