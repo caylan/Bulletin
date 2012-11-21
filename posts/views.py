2cb9dc1037e6574ebdae6ac5c84e229918dba1ee
Kyle <go4ble23@gmail.com>
2012-11-21 15:27:57 -0800

    made some progress ...

--- posts/views.py
+++ posts/views.py
@@ -9,12 +9,19 @@ from django.shortcuts import render
 from forms import CommentForm, PostForm
 from models import Comment, Post
 from groups.models import Group
+from gevent.event import Event
 
 class Posts(object):
     def __init__(self):
-        pass
+        # map group.pk -> gEvent
+        self.group_event = dict([])
 
     def comment(self, request, grpid, postid):
+        '''
+        make a comment for given post, return comment json
+        '''
+
+        # comfirm current user is a member of the group the post belongs to
         try:
             # author = the membership whose group has a membership with the post we're looking at
             comment_author = request.user.membership_set.get(group__membership__post__pk=postid)
@@ -27,10 +34,24 @@ class Posts(object):
                 comment = form.save(commit=False)
                 comment.author = comment_author
                 # TODO try catch statement
-                comment.post = Post.objects.get(pk=postid)
+                comment_post = Post.objects.get(pk=postid)
+                comment.post = comment_post
                 comment.save()
+                
+                # is anybody listening in this group?
+                # if so, alert them
+                if postid in self.group_event and not self.group_event[postid].is_set():
+                    self.group_event[postid].set()
+                    self.group_event[postid].clear()
+                
                 return HttpResponse(comment.json(), mimetype='application/json')
         return HttpResponseBadRequest()
+
+    def update(self, request, grpid):
+        '''
+        wait until a post or comment has been made
+        '''
+        pass
 
     @login_required
     def post(self, request, grpid):
@@ -47,4 +68,5 @@ class Posts(object):
 
 posts = Posts()
 comment = posts.comment
+update = posts.update
 post = posts.post
