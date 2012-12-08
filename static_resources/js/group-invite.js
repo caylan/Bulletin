58ffc171391ce20bb21ee43ddc0c4b18093f9720
Brian-Oliphant <oliphb@uw.edu>
2012-12-08 07:35:28 -0800

    Adding ajax failures

--- static_resources/js/group-invite.js
+++ static_resources/js/group-invite.js
@@ -89,6 +89,24 @@ function ajaxInvitePeople() {
 }
 
 $(document).ready(function() {
+	$(function() {
+	    $.ajaxSetup({
+		error: function(jqXHR, exception) {
+		    if (jqXHR.status === 0) {
+		    } else if (jqXHR.status == 404) {
+		        alert('Requested page not found. [404]');
+		    } else if (jqXHR.status == 500) {
+		        alert('Internal Server Error [500].');
+		    } else if (exception === 'parsererror') {
+		        alert('Requested JSON parse failed.');
+		    } else if (exception === 'timeout') {
+		        alert('Time out error.');
+		    } else if (exception === 'abort') {
+		        alert('Ajax request aborted.');
+		    }
+		}
+	    });
+	});	
 	ajaxInvitePeople();
 	addNewPerson();
 });
