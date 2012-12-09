157610e02250681213693c3af4d45ee96cdab123
Peter Weisbeck <peterweisbeck@gmail.com>
2012-12-08 22:11:48 -0800

    update for lists of notifications

--- static_resources/js/inbox.js
+++ static_resources/js/inbox.js
@@ -5,23 +5,27 @@
  * based on example from
  * http://techoctave.com/c7/posts/60-simple-long-polling-example-with-javascript-and-jquery
  */
-function update() {
-    $.ajax({url: "update/", success: function(data) {
-    	$data = $(data);
-    	if ($data.hasClass("notification")) {
-            // returned data is a proper notification.
-            var $newNotif = $data
-            var $noNotif = $('.alert.alert-warning');
-            $newNotif.hide();
-            $newNotif.find(".avatar").show();
-            $newNotif.find(".timeago").timeago().show();
 
-            // Remove the "no updates" notification.
-            removeNoUpdateWarning($noNotif);
-            $("#notifications").prepend($newNotif);
-            $newNotif.fadeIn();
-            animateResize($newPost.find(".avatar"), $($newNotif).height());
-        }
+// Pulls a list of notifications from the server and then inserts them into the inbox
+function update() {
+    $.ajax({url: "update/", success: function(data) { // Ajax retrieval
+		$.each(data, function() { // For each notification in the list
+			$data = $(this);
+			if ($data.hasClass("notification")) {
+				// returned data is a proper notification.
+				var $newNotif = $data
+				var $noNotif = $('.alert.alert-warning');
+				$newNotif.hide();
+				$newNotif.find(".avatar").show();
+				$newNotif.find(".timeago").timeago().show();
+	
+				// Remove the "no updates" notification.
+				removeNoUpdateWarning($noNotif);
+				$("#notifications").prepend($newNotif);
+				$newNotif.fadeIn();
+				animateResize($newPost.find(".avatar"), $($newNotif).height());
+			}
+		});
     }, dataType: "html", complete: update, timeout: 25000});
 }
 
